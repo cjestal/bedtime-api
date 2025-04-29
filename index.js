1925ad4c78596d1fa46d242c80866dc4405a8be3
Clint James <cjestal@gmail.com>
2025-04-29 14:33:26 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,154 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const genAI = require('@google/genai');
+const app = express()
+const qs = require('qs')
+const port = 4000
+const dotenv = require('dotenv').config();
+// const apiKey = 'AIzaSyDUFsXsBY51tkbhz5-BcIubxxRNR_RZEbA' // remove this key before pushing to GitHub
+const apiKey = process.env.API_KEY // remove this key before pushing to GitHub
+const ai = new genAI.GoogleGenAI({ apiKey });
+const cors = require('cors')
+
+const {
+    HarmBlockThreshold,
+    HarmCategory,
+    Type,
+} = require('@google/genai');
+const { get } = require('http');
+
+const config = {
+    safetySettings: [
+        {
+            category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+            threshold: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,  // Block most
+        },
+        {
+            category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+            threshold: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,  // Block most
+        },
+        {
+            category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+            threshold: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,  // Block most
+        },
+        {
+            category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+            threshold: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,  // Block most
+        },
+    ],
+    responseMimeType: 'application/json',
+    responseSchema: {
+        type: Type.OBJECT,
+        required: ["title", "moral", "characters"],
+        properties: {
+            title: {
+                type: Type.STRING,
+            },
+            moral: {
+                type: Type.STRING,
+            },
+            characters: {
+                type: Type.ARRAY,
+                items: {
+                    type: Type.OBJECT,
+                    required: ["name", "description"],
+                    properties: {
+                        name: {
+                            type: Type.STRING,
+                        },
+                        description: {
+                            type: Type.STRING,
+                        },
+                    },
+                },
+            },
+            story: {
+                type: Type.ARRAY,
+                items: {
+                    type: Type.STRING,
+                },
+            },
+        },
+    },
+};
+
+// const contents = [
+//     {
+//         role: 'user',
+//         parts: [
+//             {
+//                 text: `generate me a children's bedtime story. use the following parameters:
+//       the theme of the story is about friendship
+//       the story takes place in a forest
+//       the character species are dragon, robot, butterfly`,
+//             },
+//         ],
+//     },
+// ];
+
+app.use(bodyParser.json());
+app.use(cors());
+app.set('query parser', (str) => qs.parse(str, { comma: true }));
+
+let genCount = 0;
+const genLimit = 30;
+const genLastCountDate = new Date();
+
+const resetGenCount = () => {
+    const now = new Date();
+    if (now - genLastCountDate > 24 * 60 * 60 * 1000) {
+        genCount = 0;
+        genLastCountDate = now;
+    }
+}
+
+const genResponse = async (prompt) => {
+    const response = await ai.models.generateContent({
+        model: "gemini-2.5-flash-preview-04-17",
+        config,
+        contents: prompt,
+    });
+    return response.text;
+}
+
+app.get('/', (req, res) => {
+    res.send('Testing api!')
+})
+
+app.post('/generate', async (req, res) => {
+    resetGenCount();
+    const testPrompt = `generate me a children's bedtime story. use the following parameters:
+                        the theme of the story is about ${req.query.theme}
+                        the story takes place in ${req.query.location}
+                        the character species are ${req.query.species}`
+
+    if (genCount >= genLimit) {
+        res.send(`You have reached the limit of ${genLimit} generations per day. Please try again tomorrow.`);
+    } else {
+        getCount++;
+        const prompt = testPrompt;
+        // const prompt = req.body.prompt;
+        // const response = await genResponse(prompt);
+        res.send(testPrompt)
+    }
+})
+
+app.get('/generate', async (req, res) => {
+    resetGenCount();
+    const prompt = `generate me a children's bedtime story. use the following parameters:
+                        the theme of the story is about ${req.query.theme}
+                        the story takes place in ${req.query.location}
+                        the character species are ${req.query.species}`
+
+    if (genCount >= genLimit) {
+        res.send(`You have reached the limit of ${genLimit} generations per day. Please try again tomorrow.`);
+    } else {
+        genCount++;
+        const response = await genResponse(prompt);
+        res.send(response)
+    }
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
